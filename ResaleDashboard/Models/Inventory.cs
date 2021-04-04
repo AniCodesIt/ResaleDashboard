dce120dc8240539862dd048838e7f25382627aef
anicodesit <ani.kulka@gmail.com>
2021-04-04 18:42:31 -0400

    Added Inventory

---
+++ ResaleDashboard/Models/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResaleDashboard.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int InvID { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public decimal COG { get; set; }
+        public int StockOnHand { get; set; }
+        public string Location { get; set; }
+    }
+    public class InventoryDbContext : DbContext
+    {
+        public DbSet<Inventory> Inventories { get; set; }
+
+    }
+}
---
+++ ResaleDashboard/Models/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResaleDashboard.Models
+{
+    public class Platform
+    {
+        public int PlatformID { get; set; }
+        public string PlatformName { get; set; }
+        public decimal Fees { get; set; }
+    }
+    public class PlatformDbContext : DbContext
+    {
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
