51f8b4c5c60f65725f5bf82295aa1169e7b028e8
anicodesit <ani.kulka@gmail.com>
2021-04-24 13:46:43 -0400

    Wrote SalesByCategory and SalesByPlatform endpoints

--- ResaleDashboard/Controllers/SaleController.cs
+++ ResaleDashboard/Controllers/SaleController.cs
@@ -89,6 +89,8 @@ namespace ResaleDashboard.Controllers
             }
             return View(sale);
         }
+        [HttpGet]
+        //[ValidateAntiForgeryToken]
         // Get: Sale/Details{id}
         public ActionResult Details(int? id)
         {
@@ -103,6 +105,71 @@ namespace ResaleDashboard.Controllers
                 return HttpNotFound();
             }
             return View(sale);
+        }
+        [HttpGet]
+        //[ValidateAntiForgeryToken]
+        // Get: Sale/Details{Platform/id}
+        public ActionResult SalesbyPlatform(int? platformID)
+        {
+            if (platformID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            var query =
+                _db
+                .Sales
+                .Where(
+                    e => e.PlatformID == platformID
+                    )
+                .Select(e =>
+                    new Sale
+                    {
+                        SaleID = e.SaleID,
+                        PlatformID = e.PlatformID,
+                        InvID = e.InvID,
+                        SaleDate = e.SaleDate,
+                        SalePrice = e.SalePrice,
+                        Profit = e.Profit
+                    }
+                    );
+            if (query == null)
+            {
+                return HttpNotFound();
+            }
+            return View(query.ToList());               
+          
+        }
+        [HttpGet]
+        //[ValidateAntiForgeryToken]
+        // Get: Sale/Details{Platform/id}
+        //add ienumerable to inventory.category model
+        public ActionResult SalesbyCategory(string categoryID)
+        {
+            var InvDb = new InventoryDbContext();
+            if (categoryID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            var query = _db.Sales.Join(InvDb.Inventories, s => s.InvID, i => i.InvID, (s, i) => new { Sales = s, Inventories = i })
+                .Where(w => w.Inventories.Category == categoryID)
+                .Select(e =>
+                    new Sale
+                    {
+                        SaleID = e.Sales.SaleID,
+                        PlatformID = e.Sales.PlatformID,
+                        InvID = e.Sales.InvID,
+                        SaleDate = e.Sales.SaleDate,
+                        SalePrice = e.Sales.SalePrice,
+                        Profit = e.Sales.Profit,                       
+                    }                   
+                    );
+            if (query == null)
+            {
+                return HttpNotFound();
+            }
+            return View(query.ToList());
         }
     }
 }
